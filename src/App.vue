82c3585ed59631741b81811b1712b98cb298659d
Pieter <pieter.hiele@gmail.com>
2020-12-10 12:13:00 +0100

    some improvements

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
                         </div>
                     </div>
                     <div class="col-auto">
-                        <button type="submit" class="btn btn-primary mb-2" @click.prevent="connectServer">Connect</button>
+                        <button type="submit" class="btn btn-primary mb-2" @click.prevent="connect_server">Connect</button>
                     </div>
                 </div>
             </form>
@@ -34,6 +34,7 @@
                         <div class="card-body text-center">
                             <h5 class="card-title">{{ stats.domain_count.toLocaleString() }}</h5>
                             <p class="card-text">Domains</p>
+                            <p class="small">{{stats.domain_resolved_count.toLocaleString() }} resolved ({{ (stats.domain_resolved_count/stats.domain_count)*100 }}%)</p>
                         </div>
                     </div>
                 </div>
@@ -67,7 +68,32 @@
             <div>
                 <b-tabs>
                     <b-tab v-for="(docs, doctype) in docstore" :key="doctype" :title="docs.display_value">
-                        <b-table show-empty striped bordered :items="docs.records" :fields="docs.table.fields" :options="docs.table.options" />
+                        <div v-if="doctype == 'domains'">
+                            <br />
+                            <div class="card">
+                                <div class="card-body">
+                                    <b-form-radio v-model="docstore.domains.filter_domains" name="domain-filter" value="all">Show all</b-form-radio>
+                                    <b-form-radio v-model="docstore.domains.filter_domains" name="domain-filter" value="resolved">Resolved only</b-form-radio>
+                                    <b-form-radio v-model="docstore.domains.filter_domains" name="domain-filter" value="unresolved">Unresolved only</b-form-radio>
+                                </div>
+                            </div>
+                        </div>
+                        <br />
+                        <b-table show-empty striped bordered :busy="docs.table.isBusy" :items="records_filtered(doctype)" :fields="fields_filtered(doctype)" :options="docs.table.options">
+                            <template #table-busy>
+                                <div class="text-center">
+                                    <p>
+                                        <b-spinner class="align-middle"></b-spinner>
+                                    </p>
+                                    <p><strong>Loading...</strong></p>
+                                </div>
+                            </template>
+                            <template slot="top-row" slot-scope="{ fields }">
+                                <td v-for="field in fields" :key="field.key">
+                                    <input @change="search_table" width="100%" v-model="docs.table.filters[field.key]" placeholder="">
+                                </td>
+                            </template>
+                        </b-table>
                     </b-tab>
                 </b-tabs>
             </div>
@@ -89,7 +115,9 @@
     import {
         BTabs,
         BTab,
-        BTable
+        BTable,
+        BFormRadio,
+        BSpinner
     } from 'bootstrap-vue'
 
     import PouchDB from 'pouchdb'
@@ -101,7 +129,9 @@
         components: {
             BTable,
             BTabs,
-            BTab
+            BTab,
+            BFormRadio,
+            BSpinner
         },
         data: function() {
             return {
@@ -119,31 +149,45 @@
                 alerts: [],
                 docstore: {
                     domains: {
+                        filter_domains: 'all',
                         display_value: 'Domains',
                         records: [], // will be populated by PouchDB
                         table: {
+                            isBusy: false,
                             fields: [{
                                     key: 'id',
                                     label: 'Domain',
                                     sortable: true
                                 },
                                 {
-                                    key: 'doc.program',
-                                    label: 'Program',
+                                    key: 'doc.ips',
+                                    label: 'IPs',
                                     sortable: false
                                 },
                                 {
                                     key: 'doc.source',
                                     label: 'Source',
                                     sortable: true
+                                },
+                                {
+                                    key: 'doc.program',
+                                    label: 'Program',
+                                    sortable: false
                                 }
                             ],
+                            filters: {
+                                id: '',
+                                'doc.ips': '',
+                                'doc.source': '',
+                                'doc.program': ''
+                            }
                         }
                     },
                     ips: {
                         display_value: 'IPs',
                         records: [], // will be populated by PouchDB
                         table: {
+                            isBusy: false,
                             fields: [{
                                     key: 'id',
                                     label: 'IP',
@@ -155,23 +199,29 @@
                                     sortable: false
                                 },
                                 {
-                                    key: 'doc.program',
-                                    label: 'Program',
-                                    sortable: false
-                                },
-                                {
                                     key: 'doc.source',
                                     label: 'Source',
                                     sortable: true
+                                },
+                                {
+                                    key: 'doc.program',
+                                    label: 'Program',
+                                    sortable: false
                                 }
-                            ]
-
+                            ],
+                            filters: {
+                                id: '',
+                                'doc.domains': '',
+                                'doc.source':'',
+                                'doc.program':''
+                            }
                         }
                     },
                     urls: {
                         display_value: 'URLs',
                         records: [], // will be populated by PouchDB
                         table: {
+                            isBusy: false,
                             fields: [{
                                     key: 'id',
                                     label: 'URL',
@@ -198,27 +248,99 @@
                                     sortable: true
                                 },
                                 {
-                                    key: 'doc.program',
-                                    label: 'Program',
-                                    sortable: false
-                                },
-                                {
                                     key: 'doc.source',
                                     label: 'Source',
                                     sortable: true
+                                },
+                                {
+                                    key: 'doc.program',
+                                    label: 'Program',
+                                    sortable: false
                                 }
-                            ]
+                            ],
+                            filters: {
+                                id: '',
+                                'doc.hostname':'',
+                                'doc.port':'',
+                                'doc.status':'',
+                                'doc.content_length':'',
+                                'doc.source':'',
+                                'doc.program':''
+                            }
 
                         }
                     }
                 },
             }
         },
+        computed: {
+            fields_filtered: function() {
+                let vm = this
+                return function(doctype) {
+                    var documents = vm.docstore[doctype]
+                    var results = []
+                    if (!vm.program) {
+                        results = documents.table.fields
+                    } else {
+                        results = documents.table.fields.filter(function(field) {
+                            if (field.key != 'doc.program') return true
+                        })
+                    }
+
+                    // filter out IP addresses if we're looking at unresolved data
+                    if (doctype == 'domains' && documents.filter_domains == 'unresolved') {
+                        results = results.filter(function(field) {
+                            if (field.key != 'doc.ips') return true
+                        })
+                    }
+
+                    return results
+                }
+            },
+            
+            records_filtered: function() {
+                // only return rows that match the filter value
+                
+                let vm = this
+                
+                return function(doctype) {
+                    var documents = vm.docstore[doctype]
+                    var results = documents.records
+                    
+                    results = results.filter(function(row) {
+                        
+                        var all_fields_match = true
+                        
+                        // compare every field of the row to the filters
+                        for(var i in documents.table.fields) {
+                            var field = documents.table.fields[i].key
+                            if (field in documents.table.filters && documents.table.filters[field].length > 0) {
+                                
+                                var filter = documents.table.filters[field]
+                                
+                                console.log(documents.table.filters[field])
+                                console.log(row.field)
+                                var value = eval('row.'+field)
+                                console.log(value)
+                                if(value && !value.toString().includes(filter)) {
+                                    all_fields_match = false
+                                }
+                                // there's a filter set, compare it to the row
+                            }
+                        }
+                        
+                        return all_fields_match
+                    })
+                    
+                    return results
+                }
+            }
+        },
         created: function() {
-            if (this.couchdb) this.connectServer()
+            if (this.couchdb) this.connect_server()
         },
         methods: {
-            connectServer: function() {
+            connect_server: function() {
                 if (this.couchdb) {
                     this.db = new PouchDB(this.couchdb)
                     localStorage.setItem('couchdb', this.couchdb)
@@ -230,6 +352,7 @@
             },
             get_domains: function() {
 
+                this.docstore.domains.table.isBusy = true
                 this.docstore.domains.records = []
                 let me = this
 
@@ -239,17 +362,23 @@
                 }
 
                 if (this.program) options.key = this.program
+                var suffix = ''
+                if (this.docstore.domains.filter_domains != 'all') {
+                    suffix = '_' + this.docstore.domains.filter_domains
+                }
 
-                this.db.query('bbrf/domains', options).then(function(response) {
+                this.db.query('bbrf/domains' + suffix, options).then(function(response) {
                     for (var i = 0; i < response.rows.length; i++) {
                         me.docstore.domains.records.push(response.rows[i])
                     }
+                    me.docstore.domains.table.isBusy = false
                 }).catch(function() {
 
                 })
             },
             get_ips: function() {
 
+                this.docstore.ips.table.isBusy = true
                 this.docstore.ips.records = []
                 let me = this
 
@@ -264,12 +393,14 @@
                     for (var i = 0; i < response.rows.length; i++) {
                         me.docstore.ips.records.push(response.rows[i])
                     }
+                    me.docstore.ips.table.isBusy = false
                 }).catch(function() {
 
                 })
             },
             get_urls: function() {
                 let me = this
+                this.docstore.urls.table.isBusy = true
                 this.docstore.urls.records = []
 
                 var options = {
@@ -283,6 +414,7 @@
                     for (var i = 0; i < response.rows.length; i++) {
                         me.docstore.urls.records.push(response.rows[i])
                     }
+                    me.docstore.urls.table.isBusy = false
                 }).catch(function() {
 
                 })
@@ -312,30 +444,28 @@
                     this.stats[i] = 0;
                 }
 
+                var options = {
+                    reduce: true
+                }
+
+                if (this.program) options.key = this.program
+
                 this.db.query('bbrf/programs', {
                     reduce: true
                 }).then(function(response) {
-                    me.stats.program_count = response.rows[0].value
+                    me.stats.program_count = response.rows.length > 0 ? response.rows[0].value : 0
                 }).catch(function() {})
-                this.db.query('bbrf/domains', {
-                    reduce: true
-                }).then(function(response) {
-                    me.stats.domain_count = response.rows[0].value
+                this.db.query('bbrf/domains', options).then(function(response) {
+                    me.stats.domain_count = response.rows.length > 0 ? response.rows[0].value : 0
                 }).catch(function() {})
-                this.db.query('bbrf/domains_resolved', {
-                    reduce: true
-                }).then(function(response) {
-                    me.stats.domain_resolved_count = response.rows[0].value
+                this.db.query('bbrf/domains_resolved', options).then(function(response) {
+                    me.stats.domain_resolved_count = response.rows.length > 0 ? response.rows[0].value : 0
                 }).catch(function() {})
-                this.db.query('bbrf/urls_by_hostname', {
-                    reduce: true
-                }).then(function(response) {
-                    me.stats.url_count = response.rows[0].value
+                this.db.query('bbrf/urls_by_hostname', options).then(function(response) {
+                    me.stats.url_count = response.rows.length > 0 ? response.rows[0].value : 0
                 }).catch(function() {})
-                this.db.query('bbrf/ips', {
-                    reduce: true
-                }).then(function(response) {
-                    me.stats.ip_count = response.rows[0].value
+                this.db.query('bbrf/ips', options).then(function(response) {
+                    me.stats.ip_count = response.rows.length > 0 ? response.rows[0].value : 0
                 }).catch(function() {})
             },
             select_program: function() {
@@ -343,6 +473,7 @@
                 this.get_domains()
                 this.get_ips()
                 this.get_urls()
+                this.get_stats()
             },
             listen_for_changes: function() {
                 let vm = this
@@ -351,32 +482,44 @@
                     live: true,
                     include_docs: true
                 }).on('change', function(change) {
-                    
+
                     vm.get_stats()
-                    
-                    if((change.doc.program == this.program) || !this.program) {
+
+                    if ((change.doc.program == this.program) || !this.program) {
                         // it's a change to the program we're viewing (or we're viewing all programs)
-                        
-                        if(change.doc.type == 'domain') {
+
+                        if (change.doc.type == 'domain') {
                             vm.get_domains()
-                        } else if(change.doc.type == 'ip') {
+                        } else if (change.doc.type == 'ip') {
                             vm.get_ips()
-                        } else if(change.doc.type == 'url') {
+                        } else if (change.doc.type == 'url') {
                             vm.get_urls()
                         }
-                        
+
                     }
                     console.log(change)
                 }).on('error', function(err) {
                     // handle errors
                     console.log(err)
                 })
+            },
+            search_table: function() {
+                return 1
             }
 
+        },
+        watch: {
+            'docstore.domains.filter_domains': function() {
+                this.get_domains()
+            },
+            deep: true
         }
     }
+
 </script>
 
 <style>
     #app {}
+    td input {display: block; padding: 0; margin: 0; border: 1; width: 100%;}
+    td {margin: 0; padding: 0;}
 </style>
